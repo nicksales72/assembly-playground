ffbbb29a7bd0d890b3fb0976b1c0b56dc64760c7
Nick <77275500+nicksales72@users.noreply.github.com>
2025-05-19 07:49:10 -0400

    Update asem.s

--- hello-world/asem.s
+++ hello-world/asem.s
@@ -5,7 +5,7 @@ _start:
         # sys_write
         mov rax, 1 # invoke write syscall
         mov rdi, 1 # rdi register writes to stdout fd
-        lea rsi, [hello_world] # rdx register loads address of hello_world buffer
+        lea rsi, [hello_world] # rsi register loads address of hello_world buffer
         mov rdx, 13 # rdx register allocates 13 bytes for buffer length
         syscall
 
